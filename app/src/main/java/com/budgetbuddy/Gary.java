55f9de54ffb501358cc3db3011f953ecc2c8c327
Gary Allen <gvcallen@gmail.com>
2020-07-12 15:31:15 +0200

    Add setName

--- app/src/main/java/com/budgetbuddy/Gary.java
+++ app/src/main/java/com/budgetbuddy/Gary.java
@@ -2,9 +2,14 @@ package com.budgetbuddy;
 
 public class Gary
 {
+	String mName;
 	// Comment after re-clone
 	// Second comment from Gary
     // Comment from Sean
     // Comment gang4life
     // Shanks was here
+	public void setName(String name)
+	{
+		mName = name;
+	}
 }
