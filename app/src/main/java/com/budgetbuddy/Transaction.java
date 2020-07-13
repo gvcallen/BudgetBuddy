afdcf25da33fc8694ca54eb7c5578c2687a9fab6
Gary Allen <gvcallen@gmail.com>
2020-07-13 19:57:23 +0200

    Filled in implementation of User, Category and Transaction classes

--- app/src/main/java/com/budgetbuddy/Transaction.java
+++ app/src/main/java/com/budgetbuddy/Transaction.java
@@ -1,5 +1,47 @@
 package com.budgetbuddy;
 
+import java.time.LocalDate;
+
 public class Transaction
 {
+	private LocalDate mDate;
+	private int mAmount;
+	private String mLocation;
+
+	Transaction(LocalDate date, int amount, String location)
+	{
+		mDate = date;
+		mAmount = amount;
+		mLocation = location;
+	}
+
+	public LocalDate getDate()
+	{
+		return mDate;
+	}
+
+	public void setDate(LocalDate date)
+	{
+		mDate = date;
+	}
+
+	public int getAmount()
+	{
+		return mAmount;
+	}
+
+	public void setAmount(int amount)
+	{
+		mAmount = amount;
+	}
+
+	public String getLocation()
+	{
+		return mLocation;
+	}
+
+	public void setLocation(String location)
+	{
+		mLocation = location;
+	}
 }
--- app/src/main/java/com/budgetbuddy/User.java
+++ app/src/main/java/com/budgetbuddy/User.java
@@ -1,5 +1,65 @@
 package com.budgetbuddy;
 
+import java.util.ArrayList;
+
 public class User
 {
+	private int mId;
+	private int mIncome;
+	private String mName, mSurname;
+	private ArrayList<Category> mCategories;
+
+	User(int id, String name, String surname, int income, ArrayList<Category> categories)
+	{
+		mId = id;
+		mName = name;
+		mSurname = surname;
+		mCategories = categories;
+		mIncome = income;
+	}
+
+	public int getId()
+	{
+		return mId;
+	}
+
+	public String getName()
+	{
+		return mName;
+	}
+
+	public void setName(String name)
+	{
+		mName = name;
+	}
+
+	public String getSurname()
+	{
+		return mSurname;
+	}
+
+	public void setSurname(String surname)
+	{
+		mSurname = surname;
+	}
+
+	public void setIncome(int income)
+	{
+		mIncome = income;
+	}
+
+	public int getIncome()
+	{
+		return mIncome;
+	}
+
+	public void setCategories(ArrayList<Category> categories)
+	{
+		mCategories = categories;
+	}
+
+	public ArrayList<Category> getCategories()
+	{
+		return mCategories;
+	}
 }
