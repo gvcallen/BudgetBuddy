3ef352d445ea553b18a012db95296c6a3d9cf579
Shankar Palamootil <shanks11.palamootil@gmail.com>
2020-07-14 14:56:51 +0200

    Added a few buttons on the interface created a menu created drawables for buttons

--- app/src/main/java/com/budgetbuddy/MainActivity.java
+++ app/src/main/java/com/budgetbuddy/MainActivity.java
@@ -3,6 +3,12 @@ package com.budgetbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +18,19 @@ public class MainActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
+		//toolbar
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		//This is the method to add the settings button on the top right it fetches the menu layout in the menu directory
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_homepage,menu);
+		return true;
 	}
 }
