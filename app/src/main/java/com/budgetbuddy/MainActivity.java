100e4a814cebbbd7d75c6fa5eda1b7d9032eed9a
Shankar Palamootil <shanks11.palamootil@gmail.com>
2020-07-14 17:40:14 +0200

    Added pie chart added resource files for animations

--- app/src/main/java/com/budgetbuddy/MainActivity.java
+++ app/src/main/java/com/budgetbuddy/MainActivity.java
@@ -8,17 +8,30 @@ import android.view.MenuInflater;
 //import android.widget.Toolbar;
 import androidx.appcompat.widget.Toolbar;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity
 {
-
+	AnyChartView pieChart;
+	String[] categories = {"Food", "Rent", "Leisure", "Utilities"} ;
+	int[] amounts = {3000, 8000, 1000, 2000 } ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		//implementing piechart
+		pieChart = findViewById(R.id.piChart);
+		setupPieChart();
 
 		//toolbar
 		Toolbar toolbar = findViewById(R.id.toolbar);
@@ -32,5 +45,23 @@ public class MainActivity extends AppCompatActivity
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_homepage,menu);
 		return true;
+	}
+
+
+
+	public void setupPieChart()
+	{
+		Pie pie = AnyChart.pie();
+		List<DataEntry> dataEntries = new ArrayList<>();
+
+			for (int i=0; i<categories.length;i++)
+			{
+				dataEntries.add(new ValueDataEntry(categories[i], amounts[i]));
+			}
+
+		pie.data(dataEntries);
+		pie.title("Monthly Spending");
+		pieChart.setChart(pie);
+
 	}
 }
