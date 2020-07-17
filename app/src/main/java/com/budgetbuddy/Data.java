c0574590f4a2735f338cff8a19fa2c810563b835
Sean Muller <seanmuller2001@gmail.com>
2020-07-17 13:29:25 +0200

    finished data class

--- app/src/main/java/com/budgetbuddy/Data.java
+++ app/src/main/java/com/budgetbuddy/Data.java
@@ -1,17 +1,20 @@
 package com.budgetbuddy;
 
+import android.content.Context;
 import com.google.gson.Gson;
-
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Formatter;
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 final public class Data
 {
-    private static Formatter mFormatter;
-    private static Scanner mScanner;
     private static String json;
     private static Gson gson;
+    private static String fileName = "User.json";
 
 
     private Data()
@@ -28,53 +31,126 @@ final public class Data
     }
 
     // Returns a User object from the json file
-    public static User readUser()
+    public static User readUser(Context context)
     {
-        json = "";
         initGson();
-
-        try
-        {
-            mScanner = new Scanner(new File("User.json"));
-        }
-        catch (Exception e) {
-            System.out.println("Formatter unsuccessful");
-        }
-
-        while (mScanner.hasNext())
-        {
-            json += mScanner.next();
-        }
-        return gson.fromJson(json, User.class);
+        json = loadTextFromFile(fileName, context);
+        json=jsonFormat(json);
+        return gson.fromJson(json,User.class);
     }
     // Overwrites the json file and stores a new user object
-    public static void saveUser(User user)
+    public static void saveUser(User user, Context context)
     {
         initGson();
-        try
-        {
-            mFormatter = new Formatter("User.json");
-        }
-
-        catch (Exception e) {
-            System.out.println("Formatter unsuccessful");
-        }
         json = gson.toJson(user);
-        mFormatter.format(json);
-        mFormatter.close();
+        saveTextToFile(json,fileName,context);
     }
 
-    // Checks if the json file exists
-    public static boolean fileExists()
+    // Checks if a file exists
+    public static boolean fileExists(Context context, String filename)
     {
-        try
+        File file = context.getFileStreamPath(filename);
+        if(file == null || !file.exists())
         {
-            mScanner = new Scanner(new File("User.json"));
-            return true;
-        }
-        catch (Exception e){
             return false;
         }
+        return true;
+    }
+
+    //overwrites and saves text to a given file or creates a new file if no file exists with the given name
+    private static void saveTextToFile(String text, String fileName, Context context)
+    {
+        FileOutputStream fos = null;
+
+        try
+        {
+            fos = context.openFileOutput(fileName,context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (fos!=null)
+            {
+                try
+                {
+                    fos.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //loads text from a given file and returns it
+    private static String loadTextFromFile(String fileName, Context context)
+    {
+        FileInputStream fis = null;
+
+        try
+        {
+            fis = context.openFileInput(fileName);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+
+            String text;
+            while ((text = br.readLine()) != null)
+            {
+                sb.append(text);
+            }
+            return sb.toString();
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        finally
+        {
+            if (fis !=null)
+            {
+                try
+                {
+                    fis.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //converts a string to be compatible with json
+    private static String jsonFormat(String text)
+    {
+        text = text.replaceAll("\"","\\\"");
+        return text;
+    }
+
+    public static void setFileName(String newFileName)
+    {
+        fileName = newFileName;
+    }
+
+    public static String getFileName()
+    {
+        return fileName;
     }
 }
-        
+
