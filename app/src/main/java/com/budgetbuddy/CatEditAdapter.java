0d9c190637f76c650d1d9fdc31f70c8d1b4040a1
Sam <sam8smart@gmail.com>
2020-07-19 02:36:59 +0200

    adding categories and edit categories activities

---
+++ app/src/main/java/com/budgetbuddy/CatEditAdapter.java
@@ -0,0 +1,82 @@
+package com.budgetbuddy;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CatEditAdapter extends RecyclerView.Adapter<CatEditAdapter.CatEditViewHolder>
+{
+    private ArrayList<Category> mCatEditList;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener
+    {
+        void onDeleteClick(int positio);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener)
+    {
+        mListener = listener;
+    }
+
+    public static class CatEditViewHolder extends RecyclerView.ViewHolder
+    {
+        public TextView tvEditType;
+        public TextView tvEditBud;
+        public ImageView icDelete;
+
+        public CatEditViewHolder(View itemView, final OnItemClickListener listener)
+        {
+            super(itemView);
+            tvEditType = itemView.findViewById(R.id.tvEditType);
+            tvEditBud = itemView.findViewById(R.id.tvEditBudget);
+            icDelete = itemView.findViewById(R.id.icDelete);
+
+            icDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null)
+                    {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION)
+                        {
+                            listener.onDeleteClick(position);
+                        }
+                    }
+
+                }
+            });
+        }
+    }
+    public CatEditAdapter(ArrayList<Category> catlist) {mCatEditList = catlist;}
+
+    @NonNull
+    @Override
+    public CatEditViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View editView = LayoutInflater.from(parent.getContext()).inflate(R.layout.cat_item, parent, false);
+        CatEditAdapter.CatEditViewHolder cevh = new CatEditAdapter.CatEditViewHolder(editView, mListener);
+        return cevh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CatEditViewHolder holder, int position)
+    {
+        Category currentCatEdit = mCatEditList.get(position);
+
+        holder.tvEditType.setText(currentCatEdit.getType());
+        holder.tvEditBud.setText(currentCatEdit.getBudget());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCatEditList.size();
+    }
+}
