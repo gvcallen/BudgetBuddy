469279741a58f96d9b544e2c4403a3bc7cc77bdb
Sean Muller <seanmuller2001@gmail.com>
2020-07-21 18:52:29 +0200

    GUI updates to transaction list and category activities

--- app/src/main/java/com/budgetbuddy/TransactionListActivity.java
+++ app/src/main/java/com/budgetbuddy/TransactionListActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,6 +14,8 @@ public class TransactionListActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapterTransList;
     private RecyclerView.LayoutManager mLayoutManagerTansLists;
     private static ArrayList<Transaction> mTransList;
+    private String category;
+    private TextView tvHeading;
 
 
 
@@ -20,8 +23,13 @@ public class TransactionListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_list);
+        tvHeading = (TextView) findViewById(R.id.txtViewTransListHeading);
+
         int position = getIntent().getIntExtra("CATEGORY_POSITION",0);
+        category =MainActivity.mUser.getCategories().get(position).getType();
         mTransList = MainActivity.mUser.getCategories().get(position).getTransactions();
+        tvHeading.setText(category + " transaction list");
+
         buildRecyclerView();
     }
 
